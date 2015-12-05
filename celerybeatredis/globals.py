78a98d6c7d96393a29dad4873db7089cabda09fd
Younggun Kim <iam@younggun.kim>
2015-12-05 23:12:02 +0900

    Added bytes_to_str() to support PY3.

--- celerybeatredis/globals.py
+++ celerybeatredis/globals.py
@@ -5,6 +5,7 @@
 # use this file except in compliance with the License. You may obtain a copy
 # of the License at http://www.apache.org/licenses/LICENSE-2.0
 
+import sys
 from redis.client import StrictRedis
 from celery import current_app
 from celery.utils.log import get_logger
@@ -17,3 +18,19 @@ Couldn't add entry %r to redis schedule: %r. Contents: %r
 """
 
 logger = get_logger(__name__)
+
+PY3 = sys.version_info >= (3, 0)
+
+default_encoding = 'utf-8'
+def bytes_to_str(s):
+    if PY3:
+        if isinstance(s, bytes):
+            return s.decode(default_encoding)
+    return s
+
+def str_to_bytes(s):
+    if PY3:
+        if isinstance(s, str):
+            return s.encode(default_encoding)
+    return s
+
