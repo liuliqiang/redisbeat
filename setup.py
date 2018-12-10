aec153d307e96f9ae1ff99b893854a777925b53c
Liqiang Liu <liqianglau@outlook.com>
2018-12-10 22:15:59 +0800

    Release v1.1.0

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.0.7",
+    version="1.1.0",
 
     description="Redis Scheduler For Celery, Support Add Task Dynamic",
     long_description="",
