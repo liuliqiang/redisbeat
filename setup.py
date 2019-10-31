88446a18d673bacaa9c79057c1b184324786e2aa
Liu Liqiang <liuliqiang@smartx.com>
2019-10-31 15:48:02 +0800

    cut v1.2.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="1.2.2",
+    version="1.2.3",
 
     description="Redis Scheduler For Celery, Support Add Task Dynamic",
     long_description="",
